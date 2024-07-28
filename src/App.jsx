eda32c3d609f2c82da66dbc3ebdb68ca6d21acad
Mohammad Shoaib <xenon.9210@gmail.com>
2024-07-28 23:04:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const token = "a964a982ea6085e392dfc514d6d68478";
+    axios
+      .post("/AgreementDashboard/getAllLeads", {}, { headers: { token } })
+      .then((response) => console.log(response.data))
+      .catch((reason) => console.log(reason));
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
